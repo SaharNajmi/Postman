b3353f31e8208931afc2a2d3f19baf5ea680275e
saharnajmi <saharnajmi8089@gmail.com>
2025-08-04 15:25:08 +0200

    add ktor library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,5 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.ktor.client.core)
 }
