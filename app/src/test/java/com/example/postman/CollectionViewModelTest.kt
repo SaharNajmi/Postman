857a642886576f797f365afd756efb206d5b0994
saharnajmi <saharnajmi8089@gmail.com>
2025-11-06 13:32:48 +0100

    test for collection viewmodel

--- app/src/test/java/com/example/postman/CollectionViewModelTest.kt
+++ app/src/test/java/com/example/postman/CollectionViewModelTest.kt
@@ -1,15 +1,24 @@
 package com.example.postman
 
+import com.example.postman.common.utils.formatDate
+import com.example.postman.domain.model.Collection
+import com.example.postman.domain.model.ExpandableHistoryItem
 import com.example.postman.domain.repository.CollectionRepository
 import com.example.postman.presentation.collection.CollectionViewModel
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
+import java.time.LocalDate
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class CollectionViewModelTest {
@@ -23,8 +32,35 @@ class CollectionViewModelTest {
         collectionRepository = mockk<CollectionRepository>()
         viewModel = CollectionViewModel(collectionRepository, testDispatcher)
     }
+
     @After
     fun teardown() {
         Dispatchers.resetMain()
     }
+
+    @Test
+    fun `toggleExpanded flips isExpanded by given collectionId`() = runTest {
+        val collections = listOf(
+            Collection(collectionId = "12", collectionName = "library"),
+            Collection(collectionId = "13", collectionName = "book"),
+            Collection(collectionId = "14", collectionName = "dictionary"),
+        )
+
+        coEvery { collectionRepository.getAllCollections() } returns collections
+        viewModel.getCollections()
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        viewModel.toggleExpanded("13")
+        viewModel.collections.value[1].isExpanded shouldBe true
+
+        viewModel.toggleExpanded("12")
+        viewModel.collections.value[0].isExpanded shouldBe true
+
+        viewModel.toggleExpanded("14")
+        viewModel.collections.value.forEach { it.isExpanded shouldBe true }
+
+        viewModel.toggleExpanded("12")
+        viewModel.collections.value[0].isExpanded shouldBe false
+    }
+
 }
